d0a1bcad2e04c6caec55006cd1b15c0853769c74
giihga <102439784+giihga@users.noreply.github.com>
2022-04-16 17:50:43 -0300

    Add files via upload

---
+++ aulaquatro.py
@@ -0,0 +1,8 @@
+nome = input('Digite seu nome? ')
+dia = input('Dia: ')
+mes = input('Mês: ')
+ano = input('Ano: ')
+
+data = nome + dia + mes + ano
+
+print('Ola {} você nasceu no dia  {} de {} de {}'.format(nome, dia, mes, ano))
