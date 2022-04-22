214e52635dae1ec1b326a99db9d1507ee4e25bb6
giihga <102439784+giihga@users.noreply.github.com>
2022-04-22 09:41:35 -0300

    Delete aulaquatro.py

--- aulaquatro.py
+++
@@ -1,8 +0,0 @@
-nome = input('Digite seu nome? ')
-dia = input('Dia: ')
-mes = input('Mês: ')
-ano = input('Ano: ')
-
-data = nome + dia + mes + ano
-
-print('Ola {} você nasceu no dia  {} de {} de {}'.format(nome, dia, mes, ano))
